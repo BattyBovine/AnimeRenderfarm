3ac1ab387c373765a9018bf65c62fd7e05441cfa
BattyBovine <github@battybovine.com>
2011-05-27 18:35:27 -0400

    Altered "Organization Domain" to match the website for this application.

--- main.cpp
+++ main.cpp
@@ -26,7 +26,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
 
     a.setOrganizationName("Batty Bovine Productions, LLC");
-    a.setOrganizationDomain("battybovine.com");
+    a.setOrganizationDomain("animerenderfarm.battybovine.com");
     a.setApplicationName("Anime Renderfarm");
     a.setApplicationVersion("0.1");
 
